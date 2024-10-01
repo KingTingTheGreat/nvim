2443a0f96e8281107070822532c7590e5be52401
Jeffrey <kingtingthegreat@gmail.com>
2024-10-01 16:44:20 -0400

    fix typo

--- init.lua
+++ init.lua
@@ -123,7 +123,7 @@ require("conform").setup({
 		javascript = { "prettierd", "prettier", stop_after_first = true },
 		javascriptreact = { "prettierd", "prettier", stop_after_first = true },
 		typesript = { "prettierd", "prettier", stop_after_first = true },
-		typesriptreact = { "prettierd", "prettier", stop_after_first = true },
+		typescriptreact = { "prettierd", "prettier", stop_after_first = true },
 	},
 	format_on_save = {
 		timeout_ms = 500,
